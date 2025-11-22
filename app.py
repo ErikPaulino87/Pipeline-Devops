ef9ab9fae5201af9758c89e17d2efb3c20562a2b
erikpaulino87 <eriktertuliano9@gmail.com>
2025-11-22 16:00:10 -0300

    teste 4

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
 import os
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 CORS(app)
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'dev_secret_key')
 jwt = JWTManager(app)
 
-# ---- Handlers do JWT para mensagens claras ----
-@jwt.unauthorized_loader
-def missing_token_callback(err_str):
-    return jsonify({"msg": "Missing Authorization Header", "error": err_str}), 401
+SWAGGER_URL = '/swagger'
+API_DOC_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_DOC_URL)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-@jwt.invalid_token_loader
-def invalid_token_callback(err_str):
-    return jsonify({"msg": "Invalid token", "error": err_str}), 422
-
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify({"msg": "Token has expired"}), 401
-
-# ---- Rotas da API ----
 @app.route('/')
 def home():
     return jsonify(message="API is running")
@@ -40,37 +32,6 @@ def login():
 @jwt_required()
 def protected():
     return jsonify(message="Protected route")
-
-@app.route('/docs')
-def swagger_ui():
-    html = """
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8"/>
-        <title>Swagger UI</title>
-        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css"/>
-      </head>
-      <body>
-        <div id="swagger-ui"></div>
-        <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js"></script>
-        <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-standalone-preset.js"></script>
-        <script>
-        window.onload = function() {
-          const ui = SwaggerUIBundle({
-            url: "/static/swagger.json",
-            dom_id: '#swagger-ui',
-            presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-            layout: "StandaloneLayout",
-            docExpansion: "none"
-          })
-          window.ui = ui
-        }
-        </script>
-      </body>
-    </html>
-    """
-    return Response(html, mimetype='text/html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 1313))
