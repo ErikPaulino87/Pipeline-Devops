7892676205f51386351b2dafce5e002608afcf3b
erikpaulino87 <eriktertuliano9@gmail.com>
2025-11-22 15:35:30 -0300

    Alteração do swagger.json e app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'dev_secret_key')
-
 jwt = JWTManager(app)
 
 SWAGGER_URL = '/swagger'
